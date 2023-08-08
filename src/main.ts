9de6487161ba4ce37fd5c2f69b72af56020dcdd2
Danniel Navas <dannielnavas@gmail.com>
2023-08-07 20:37:16 -0500

    ✨ enable cors

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  app.enableCors();
+
+  await app.listen(3001);
 }
 bootstrap();
